c445a9b4bcdec8afd7aa7bdb9567a33d807c6c60
CreativeDirectors <128590807+CreativeDirectors@users.noreply.github.com>
2023-07-13 14:44:15 +0200

    Update shooting-star-script-4.js
    new

--- CD-website-shooting-Star/shooting-star-script-4.js
+++ CD-website-shooting-Star/shooting-star-script-4.js
@@ -1431,10 +1431,10 @@
                             random.push(Math.random());
                         }
 
-                        geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-                        geometry.addAttribute('mouse', new THREE.Float32BufferAttribute(mouse, $a87C$var$MOUSE_ATTRIBUTE_COUNT));
-                        geometry.addAttribute('aFront', new THREE.Float32BufferAttribute(aFront, $a87C$var$FRONT_ATTRIBUTE_COUNT));
-                        geometry.addAttribute('random', new THREE.Float32BufferAttribute(random, 1));
+                        geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+                        geometry.setAttribute('mouse', new THREE.Float32BufferAttribute(mouse, $a87C$var$MOUSE_ATTRIBUTE_COUNT));
+                        geometry.setAttribute('aFront', new THREE.Float32BufferAttribute(aFront, $a87C$var$FRONT_ATTRIBUTE_COUNT));
+                        geometry.setAttribute('random', new THREE.Float32BufferAttribute(random, 1));
                         var $puli$$interop$default = $parcel$interopDefault($puli$exports);
                         var $v3o$$interop$default = $parcel$interopDefault($v3o$exports);
                         var material = this.material = new THREE.RawShaderMaterial({
@@ -1787,7 +1787,7 @@
                         var texture = new THREE.Texture(textCanvas);
                         texture.needsUpdate = true;
                         texture.minFilter = THREE.LinearFilter;
-                        var geometry = new THREE.PlaneBufferGeometry(width, height);
+                        var geometry = new THREE.BufferGeometry(width, height);
                         var uniforms = {
                             map: {
                                 value: texture
